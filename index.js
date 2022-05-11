ca286918fdfec711499cf90dda9ee2424403edfe
Colin Brooks <mcbrooks@pm.me>
2022-05-10 20:28:01 -0400

    completed javascript & updated classes for use

--- index.js
+++ index.js
@@ -1,57 +1,77 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
 
 var employeeData = []
 
-const initPrompts = [
-    {
-      type: "list",
-      name: "initManager",
-      message: "This command-line application will be used to build a team roster and an accompanying HTML page displaying the team members' information. It must be started with the team Manager's information first. Would you like to enter the Manager's information?",
-      choices: ['Yes', 'No']
-    },
-    {
-      type: "input",
-      name: "employeeName",
-      message: "What is the Manager's name?",
-      when(answers) {
-        return answers.initManager === 'Yes'
+function askInitPrompts() {
+  const initPrompts = [
+      {
+        type: "list",
+        name: "initManager",
+        message: "This command-line application will be used to build a team roster and an accompanying HTML page displaying the team members' information. It must be started with the team Manager's information first. Would you like to enter the Manager's information?",
+        choices: ['Yes', 'No']
+      },
+      {
+        type: "input",
+        name: "employeeName",
+        message: "What is the Manager's name?",
+        when(answers) {
+          return answers.initManager === 'Yes'
+        }
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is the Manager's ID?",
+        when(answers) {
+          return answers.initManager === 'Yes'
+        }
+      },
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is the Manager's email address?",
+        when(answers) {
+          return answers.initManager === 'Yes'
+        }
+      },
+      {
+        type: "input",
+        name: "employeeOfficeNum",
+        message: "What is the Manager's office number?",
+        when(answers) {
+          return answers.initManager === 'Yes'
+        }
+      },
+      {
+        type: "list",
+        name: "nextEmployee",
+        message: "Would you like to add an additional team member?",
+        choices: ['Yes', 'No - Finish & Generate HTML'],
+        when(answers) {
+          return answers.initManager === 'Yes'
+        }
       }
-    },
-    {
-      type: "input",
-      name: "employeeID",
-      message: "What is the Manager's ID?",
-      when(answers) {
-        return answers.initManager === 'Yes'
+    ]
+    return inquirer.prompt(initPrompts).then((answers) => {
+      const manager = new Manager(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.employeeOfficeNum)
+      employeeData.push(manager)
+      console.log(employeeData)
+      if (answers.nextEmployee === 'Yes') {
+        // employeeData.push(answers)
+        return askEmployeePrompts()
       }
-    },
-    {
-      type: "input",
-      name: "employeeEmail",
-      message: "What is the Manager's email address?",
-      when(answers) {
-        return answers.initManager === 'Yes'
+      else {
+        // employeeData.push(answers)
+        console.log(employeeData)
+        writeHTML()
       }
-    },
-    {
-      type: "input",
-      name: "employeeOfficeNum",
-      message: "What is the Manager's office number?",
-      when(answers) {
-        return answers.initManager === 'Yes'
-      }
-    },
-    {
-      type: "list",
-      name: "nextEmployee",
-      message: "Would you like to add an additional team member?",
-      choices: ['Yes', 'No - Finish & Generate HTML'],
-      when(answers) {
-        return answers.initManager === 'Yes'
-      }
-    },
-  ]
+    })
+    
+}
     
 function askEmployeePrompts() {
   const employeePrompts = [
@@ -111,86 +131,113 @@ function askEmployeePrompts() {
   ]
   
   return inquirer.prompt(employeePrompts).then((answers) => {
+    if (answers.employeeType === 'Engineer') {
+      const engineer = new Engineer(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.employeeGithub)
+      employeeData.push(engineer)
+    }
+    if (answers.employeeType === 'Intern') {
+      const intern = new Intern(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.employeeSchool)
+      employeeData.push(intern)
+    }
     if (answers.nextEmployee === 'Yes') {
-      employeeData.push(answers)
+      // employeeData.push(answers)
+      console.log(employeeData)
       return askEmployeePrompts()
     }
     else {
-      employeeData.push(answers)
+      // employeeData.push(answers)
+      console.log(employeeData)
       writeHTML()
     }
   })
+}
+
+function generateHTML() {
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Team Viewer</title>
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid" style="background-color: red;" id='jumbotron'>
+        <div class="container">
+            <h1 class="text-center" style="color: white;">My Team</h1>
+        </div>
+    </div>
+    
+`
+}
+
+function generateCard(name, position, id, email, special) {
+return `
+<div style="margin-top: 20px">
+  <div class="mx-auto">
+    <div class="card bg-light" style="width: 30rem; margin-bottom: 20px;">
+      <div class="card-header" style="background-color: blue;">
+        <h2 class="card-text text-center" style="margin-top: 20px; color: white;" id="Name">${name}</h2>
+        <h4 class="card-text text-center" style="margin-bottom: 20px; color: white" id="Position">${position}</h3>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item" id="ID">ID: ${id}</li>
+          <li class="list-group-item" id="Email">Email: ${email}</li>
+          <li class="list-group-item" id="Special">${special}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>`
+}
+
+// function endHTML() {
+// return 
+// }
+
+function writeHTML() {
+  var begHTML = generateHTML()
+  const filename = `index.html`;
+  
+  for (let i = 0; i < employeeData.length; i++) {
+    if (employeeData[i].getRole() === "Manager") {
+      var name = employeeData[i].name
+      var position = employeeData[i].getRole()
+      var id = employeeData[i].id
+      var email = employeeData[i].email
+      var special = ("Office Number: " + employeeData[i].officeNumber)
+      begHTML = begHTML.concat(generateCard(name, position, id, email, special))
+    }
+    if (employeeData[i].getRole() === "Engineer") {
+      var name = employeeData[i].name
+      var position = employeeData[i].getRole()
+      var id = employeeData[i].id
+      var email = employeeData[i].email
+      var special = ("Github: " + employeeData[i].github)
+      begHTML = begHTML.concat(generateCard(name, position, id, email, special))
+
+    }
+    if (employeeData[i].getRole() === "Intern") {
+      var name = employeeData[i].name
+      var position = employeeData[i].getRole()
+      var id = employeeData[i].id
+      var email = employeeData[i].email
+      var special = ("School: " + employeeData[i].school)
+      begHTML = begHTML.concat(generateCard(name, position, id, email, special))
+
+    }
+  }
+  const endHTML = `</body></html>`
+  html = (begHTML + endHTML)
+  fs.writeFile(filename, html, (err) =>
+    err ? console.log(err) : console.log(`Successfully created HTML file '${filename}'!`)
+  )
 }
 
 function getAnswers() {
-  return inquirer.prompt(initPrompts).then((answers) => {
-    if (answers.nextEmployee === 'Yes') {
-      employeeData.push(answers)
-      return askEmployeePrompts()
-    }
-    else {
-      employeeData.push(answers)
-      writeHTML()
-    }
-  })
+  return askInitPrompts()
 }
 
-function writeHTML() {
-  const html = generateHTML(employeeData)
-  const filename = `index.html`;
-  fs.writeFile(filename, html, (err) =>
-    err ? console.log(err) : console.log(`Successfully created HTML file '${filename}'!`))
-  })
-}
-
+// function to begin prompts
 getAnswers()
-
-
-  .then((data) => {
-    console.log(data);
-    const readme = generateHTML(data)
-    const filename = `index.html`;
-    fs.writeFile(filename, html, (err) =>
-      err ? console.log(err) : console.log(`Successfully created HTML file '${filename}'!`))
-    })
-
-  function generateHTML(data) {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <title>Team Viewer</title>
-    </head>
-    <body>
-        <div class="jumbotron jumbotron-fluid" style="background-color: red;">
-            <div class="container">
-                <h1 class="text-center" style="color: white;">My Team</h1>
-            </div>
-        </div>
-        
-    </body>
-    </html>`
-    }
-
-    function generateCard(data) {
-      return `
-      <div class="row" style="margin-top: 20px">
-        <div class="mx-auto">
-          <div class="card bg-light" style="width: 30rem; margin-bottom: 20px;">
-            <div class="card-header" style="background-color: blue;">
-              <h2 class="card-text text-center" style="margin-top: 20px; color: white;" id="Name">Employee's Name</h2>
-              <h4 class="card-text text-center" style="margin-bottom: 20px; color: white" id="Position">Employee's Position</h3>
-            </div>
-            <div class="card-body">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item" id="ID">ID: </li>
-                <li class="list-group-item" id="Email">Email: </li>
-                <li class="list-group-item" id="Special">Office Num/Github/School: </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>`
-    }
